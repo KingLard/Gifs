96b444f73706e9733cba8e334bacdd1a7b09e500
Brett Landes <brettl0407@gmail.com>
2020-03-12 14:05:35 -0400

    added a function to create new buttons and to prevent an animal button from being repeated. also retrieved all info needed from api in promise function

--- assets/app.js
+++ assets/app.js
@@ -26,8 +26,42 @@ function selectedButton(animal, limitNumber) {
     }).then(function (response) {
         console.log(response);
 
+        previoustopic = animal;
+        var responselist = response.data;
+        currentsearch = responselist;
+
+        for (var i = 0; i < responselist.length; i++) {
+            responselist[i].still = true;
+            var rating = (responselist[i].rating);
+            var title = responselist[i].title;
+            var mp4 = (responselist[i].images.downsized_still.url);
+            var gifCont = $("<div listindex='" + i + "' still='true' class='searchresult'><h5 class='rating'>" + rating + "</h5><img width='200px' height='200px' class='mp4' src='" + mp4 + "'><p class='rating' >" + title + "</p></div>")
+            $("#gif-container").append(gifCont);
+        }
      
     });
 };
+
+// This function will create a new button append to our original array of buttons, prevents the user from repeating an animal
+
+function newButton(value) {
+    var found = false;
+    for (var i = 0; i < topics.length; i++) {
+        if (topics[i] === value) {
+            found = true;
+        }
+    }
+    if (found) {
+        alert("Repeated Animal");
+    }
+    else if (value == false) {
+        alert("Oops! you should add an Animal");
+    }
+    else {
+        topics.push(value);
+        var addedButton = $("<input type='button'class='defined-button' value='" + value + "'></input>");
+        $(".col-sm-12").append(addedButton);
+    }
+}
 
 
